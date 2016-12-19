899158bac5f215b3205c8b32cccd58ff53d4a6d3
Lior Inbar <liorinbar3@gmail.com>
2016-12-19 11:32:12 +0200

    rename variables

--- docs_triples.py
+++ docs_triples.py
@@ -2,9 +2,9 @@ import triples_creator
 import re
 
 url="http://www.cs.technion.ac.il/research-labs/"
-querry1='//ul[2]/li'
-querry2='/a/text()'
-querry3='/a/@href'
+query1='//ul[2]/li'
+query2='/a/text()'
+query3='/a/@href'
 output =  open('docs_output.txt', 'w')
 format1 = 'http://www.cs.technion.ac.il/research-labs/@ a foaf:Group'
 format2 ='http://www.cs.technion.ac.il/research-labs/@ foaf:name @@'  
@@ -25,9 +25,9 @@ func1 = group_func
 func2 = name_func
 func3 = hompage_func
 
-triples_creator.create_triples_3(url,format1,output,querry1,querry2,func1,0,0)	
-triples_creator.create_triples_3(url,format2,output,querry1,querry2,func1,querry2,func2)
-triples_creator.create_triples_3(url,format3,output,querry1,querry2,func1,querry3,func3)
+triples_creator.create_triples_3(url,format1,output,query1,query2,func1,0,0)	
+triples_creator.create_triples_3(url,format2,output,query1,query2,func1,query2,func2)
+triples_creator.create_triples_3(url,format3,output,query1,query2,func1,query3,func3)
 
 
 
