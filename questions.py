d4c3b46dfd85ee67d1ebe149c491587f4918a59a
LiorInbar <liorinbar3@gmail.com>
2017-04-03 06:57:22 +0300

    Add files via upload

--- questions.py
+++ questions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import triples_creator_3
+import marom
 
 import urllib
 from lxml import html
@@ -15,65 +15,73 @@ import sys
 
 
 
+def some_encoding_issues(text):
+	return re.sub(b'\xc3\x97\xc2',b'\xd7',unicode(text).encode('utf-8')).decode('utf-8')
 
 def questions_not_exist_check():
-	page = urllib.urlopen('http://www.yeshiva.org.il/ask/'+str(index)).read()
-	tree = etree.HTML(page)
-	subejct_elements = tree.xpath('//p[@class="content"]//text()')
-	if(len(subejct_elements)==0):
+	check = page.evaluate_xpath('//p[@class="content"]//text()')
+	if(len(check)==0):
 		return True
 	return False
 
-def bad_pages_check():
-	page = urllib.urlopen('http://www.yeshiva.org.il/ask/'+str(index)).read()
-	tree = etree.HTML(page)
-	subejct_elements = tree.xpath('//p[@class="content"]//text()')
-	if subejct_elements[0]!=u'שאלה:':
-		return True
-	return False
 
 def type_triple():
-	questions.triples.append({})
-	questions.triples[len(questions.triples)-1]['subject']='<http://www.yeshiva.org.il/ask/'+str(index)+'>'
-	questions.triples[len(questions.triples)-1]['predicate']='<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'
-	questions.triples[len(questions.triples)-1]['object']='<http://jbs.technion.ac.il/resource/Question>'
+	page.triples.append({})
+	page.triples[len(page.triples)-1]['subject']='<'+page.get_url()+'>'
+	page.triples[len(page.triples)-1]['predicate']='<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'
+	page.triples[len(page.triples)-1]['object']='<http://jbs.technion.ac.il/resource/Question>'
 
 def title_triple():
-	questions.set_subject_query('//h1/text()')
-	questions.set_predicate('<http://jbs.technion.ac.il/resource/title>')
-	questions.set_object_query('/text()')
+	page.set_subject_query('//h1/text()')
+	page.set_predicate('<http://jbs.technion.ac.il/resource/title>')
+	page.set_object_query('/text()')
 	def f(param):
-		return '<http://www.yeshiva.org.il/ask/'+str(index)+'>'
+		return '<'+page.get_url()+'>'
 	func = f
 	def f2(str):
 		return '"'+str+'"'
 	func2 = f2
-	questions.set_subject_func(func)
-	questions.set_object_func(func2)
-	questions.create_triples()
+	page.set_subject_func(func)
+	page.set_object_func(func2)
+	page.create_triples()
+	page.triples[len(page.triples)-1]['object']=some_encoding_issues(page.triples[len(page.triples)-1]['object'])
+
 
 def rabbi_triple():
-	questions.set_subject_query('//h2/a')
-	questions.set_predicate('<http://jbs.technion.ac.il/resource/rabbi>')
-	questions.set_object_query('/@href')
+	page.set_subject_query('//h2/a/text()')
+	page.set_predicate('<http://jbs.technion.ac.il/resource/rabbiName>')
+	page.set_object_query('/text()')
 
 	def f3(str):
-		return '<'+str+'>'
+		return '"'+str+'"'
 	func3 = f3
-	questions.set_object_func(func3)
-	questions.create_triples()
+	page.set_object_func(func3)
+	page.create_triples()
+	page.triples[len(page.triples)-1]['object']=some_encoding_issues(page.triples[len(page.triples)-1]['object'])
+
+
+def date_triple():
+	page.set_subject_query('//div[@class="quesDate"]/text()')
+	page.set_predicate('<http://jbs.technion.ac.il/resource/date>')
+	page.set_object_query('/text()')
+
+	def f4(str):
+		return '"'+str+'"'
+	func4 = f4
+	page.set_object_func(func4)
+	page.create_triples()
+	page.triples[len(page.triples)-1]['object']=some_encoding_issues(page.triples[len(page.triples)-1]['object'])
+
 
 
 
 def question_and_answer_text():
-	page = urllib.urlopen('http://www.yeshiva.org.il/ask/'+str(index)).read()
-	tree = etree.HTML(page)
-	subejct_elements = tree.xpath('//p[@class="content"]//text()')
+	subejct_elements = page.evaluate_xpath('//p[@class="content"]//text()')
 	question = ''
 	answer = ''
 	i=1
 	a=u'תשובה:'
-	while subejct_elements[i]!=a:#.encode('utf-8')
+	while i < len(subejct_elements) and some_encoding_issues(subejct_elements[i])!=a:#.encode('utf-8')
 		question = question+subejct_elements[i]
 		i=i+1
 	i=i+1
@@ -94,26 +102,26 @@ def question_and_answer_text():
 			j=j+100
 		else:
 			j=j+1
-	questions.triples.append({})
-	questions.triples[len(questions.triples)-1]['subject']='<http://www.yeshiva.org.il/ask/'+str(index)+'>'
-	questions.triples[len(questions.triples)-1]['predicate']='<http://jbs.technion.ac.il/resource/question_text>'
-	questions.triples[len(questions.triples)-1]['object']='"'+question+'"'
+	page.triples.append({})
+	page.triples[len(page.triples)-1]['subject']='<'+page.get_url()+'>'
+	page.triples[len(page.triples)-1]['predicate']='<http://jbs.technion.ac.il/resource/questionText>'
+	page.triples[len(page.triples)-1]['object']='"'+some_encoding_issues(question)+'"'
 
-	questions.triples.append({})
-	questions.triples[len(questions.triples)-1]['subject']='<http://www.yeshiva.org.il/ask/'+str(index)+'>'
-	questions.triples[len(questions.triples)-1]['predicate']='<http://jbs.technion.ac.il/resource/answer_text>'
-	questions.triples[len(questions.triples)-1]['object']='"'+answer+'"'
+	page.triples.append({})
+	page.triples[len(page.triples)-1]['subject']='<'+page.get_url()+'>'
+	page.triples[len(page.triples)-1]['predicate']='<http://jbs.technion.ac.il/resource/answerText>'
+	page.triples[len(page.triples)-1]['object']='"'+some_encoding_issues(answer)+'"'
 
 def boring_technical_stuff():
 	arr = []
 	j=0
-	for i in range(len(questions.triples)):
-		if questions.triples[i]['object'] != '<http://jbs.technion.ac.il/resource/Question>':
-			arr.append(questions.triples[i]['object'])
-			questions.triples[i]['object']='"zz'+str(j)+'zz"'
+	for i in range(len(page.triples)):
+		if page.triples[i]['object'] != '<http://jbs.technion.ac.il/resource/Question>':
+			arr.append(page.triples[i]['object'])
+			page.triples[i]['object']='"zz'+str(j)+'zz"'
 			j=j+1
 
-	questions.turtle('questions_output.txt')
+	page.turtle('questions_output.txt')
 
 	with open('questions_output.txt','r') as f:
 		text = f.read()
@@ -125,27 +133,23 @@ def boring_technical_stuff():
 	f.write(text.encode('utf-8'))
 	f.close()
 	for line in fileinput.input('questions_output.txt', inplace=True):
-		print(string.replace(line,'foaf:','jbo:').rstrip()) 
+		print(string.replace(line,'foaf:','jbr:').rstrip()) 
 	f.close()
 
-questions = triples_creator_3.page()
+page = marom.page()
 index=1
 questions_not_exist = open('questions_not_exist.txt','w')
-bad_pages = open('bad_pages.txt','w')
+count = open('page_count.txt','w')
 
 while index <= 100:
-	questions.set_url('http://www.yeshiva.org.il/ask/'+str(index))
-
+	page.set_url('http://www.yeshiva.org.il/ask/'+str(index))
+	count.write(str(index)+'\n')
 	if questions_not_exist_check():
 		questions_not_exist.write(str(index)+'\n')
 		index=index+1
 		continue
-
-	if bad_pages_check():
-		bad_pages.write(str(index)+'\n')
-		index=index+1
-		continue
-
+	
+	
 	#inserting the question class triple
 	type_triple()
 
@@ -157,6 +161,9 @@ while index <= 100:
 
 	rabbi_triple()
 
+	#date triples
+	date_triple()
+
 	#question and answer text - the ugly part
 	question_and_answer_text()
 
@@ -164,10 +171,8 @@ while index <= 100:
 	index=index+1
 
 questions_not_exist.close()
-bad_pages.close()
+count.close()
 
 boring_technical_stuff()
 
-'''nt = open('test2.txt','w')
-nt.write(questions.triples[1]['object'].encode('utf-8'))'''
 
